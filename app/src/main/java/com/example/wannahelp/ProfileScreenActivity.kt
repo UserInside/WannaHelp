e9583fa090dd2cb555f46fc6f46a3932e45ff457
Voronov Igor <voronov.i.v@mail.ru>
2025-02-22 19:47:20 +0300

    tmp

--- app/src/main/java/com/example/wannahelp/ProfileScreenActivity.kt
+++ app/src/main/java/com/example/wannahelp/ProfileScreenActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class ProfileScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_profile_screen)
 
+
+        //bottomNavigation.selectedItemId = R.id.page_2
     }
 }
