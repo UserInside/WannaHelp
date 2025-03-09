cbc2f1de0d65c24d9f55ead91fc5b5b1df82c828
Voronov Igor <voronov.i.v@mail.ru>
2025-03-09 19:55:44 +0300

    add Event Details Screen

---
+++ app/src/main/java/com/example/wannahelp/EventDetailsScreenFragment.kt
@@ -0,0 +1,30 @@
+package com.example.wannahelp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class EventDetailsScreenFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_event_details_screen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        requireActivity().apply{
+            title = getString(R.string.tv_title_event_details)
+            actionBar?.setDisplayShowHomeEnabled(true)
+        }
+    }
+
+    companion object {
+           fun newInstance() = EventDetailsScreenFragment()
+    }
+}
