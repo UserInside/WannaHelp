8b735dc9828fd2f8e95a91fe857c105d752801f8
Igor Voronov <voronov.i.v@mail.ru>
2025-04-23 14:55:39 +0300

    fixed filter screen

--- app/src/main/java/com/example/wannahelp/newsScreen/newsFilterScreen/NewsFilterViewModel.kt
+++ app/src/main/java/com/example/wannahelp/newsScreen/newsFilterScreen/NewsFilterViewModel.kt
@@ -31,43 +31,29 @@ class NewsFilterViewModel(application: Application) : AndroidViewModel(applicati
     @SuppressLint("StaticFieldLeak")
     private val ctx = application.applicationContext
 
-    override fun onCleared() {
-        Log.i("DSTORE", "onCleared cats listToShow ${listOfCategoryFiltersToShow.value}")
-        Log.i("DSTORE", "onCleared cats set ${setOfChosenCategories.value}")
-        super.onCleared()
-    }
-
     private val initialFilterList = getInitialFilterList(application).toList()
 
     val setOfChosenCategories = getSelectedCategoriesSet().filterNotNull().stateIn(
         viewModelScope, SharingStarted.Eagerly, initialFilterList.map { it.category.name }.toSet()
     )
 
-    val listOfCategoryFiltersToShow =
-        MutableStateFlow<List<FilterCategoryCard>>(emptyList())
+    val listOfCategoryFiltersToShow = MutableStateFlow<List<FilterCategoryCard>>(emptyList())
 
     private var tmpSetOfFilteredCategoriesToSave = mutableSetOf<String>()
 
     init {
         viewModelScope.launch {
-            Log.i("DSTORE", "--1- onInit FilterScreen set ${setOfChosenCategories.value}")
-            Log.i("DSTORE", "--2- onInit FilterScreen ${getSelectedCategoriesSet().first()}")
-            Log.i("DSTORE", "--3- onInit FilterScreen set ${setOfChosenCategories.value}")
-
+            getSelectedCategoriesSet().first() //почему без этого не работает? потому что эмитит элемент?
             tmpSetOfFilteredCategoriesToSave.clear()
-            setOfChosenCategories.value?.let {
-                tmpSetOfFilteredCategoriesToSave.addAll(it)
-            }
+            tmpSetOfFilteredCategoriesToSave.addAll(setOfChosenCategories.value)
+
             updateLTS()
         }
     }
 
     private fun updateLTS() {
-        Log.i("DSTORE", "-- upd LTS set ${setOfChosenCategories.value}")
-
-        listOfCategoryFiltersToShow.value = initialFilterList
-            .map { item ->
-                if (setOfChosenCategories.value?.contains(item.category.toString()) == true) {
+        listOfCategoryFiltersToShow.value = initialFilterList.map { item ->
+                if (setOfChosenCategories.value.contains(item.category.toString()) == true) {
                     item.copy(isChecked = true)
                 } else {
                     item.copy(isChecked = false)
@@ -75,56 +61,47 @@ class NewsFilterViewModel(application: Application) : AndroidViewModel(applicati
             }
     }
 
-    fun getSelectedCategoriesSet(): Flow<Set<String>?> {
-        Log.i("DSTORE", "g- try get set from ds")
-        return ctx.datastore.data.map { preference ->
-            preference[stringSetPreferencesKey(CHOSEN_CATEGORIES)]
-        }.also {
-            Log.i("DSTORE", "g- loaded from ds")
-        }
+    fun getSelectedCategoriesSet(): Flow<Set<String>?> = ctx.datastore.data.map { preference ->
+        preference[stringSetPreferencesKey(CHOSEN_CATEGORIES)]
     }
 
     suspend fun saveChosenCategories() {
-        Log.i("DSTORE", "try save set to ds $tmpSetOfFilteredCategoriesToSave")
         try {
             ctx.datastore.edit { prefs ->
                 prefs[stringSetPreferencesKey(CHOSEN_CATEGORIES)] = tmpSetOfFilteredCategoriesToSave
             }
-            Log.i("DSTORE", "Successfully saved (even after cancellation)")
         } catch (e: Exception) {
             Log.e("DSTORE", "Save failed", e)
         }
     }
 
-    fun addNewsItemToFilter(category: String) {
-        Log.i("DSTORE", "add tmpSet ${setOfChosenCategories.value} ")
+    fun addNewsItemToFilter(category: String) =
         tmpSetOfFilteredCategoriesToSave.add(category)
-    }
+
 
     fun removeNewsItemFromFilter(category: String) =
         tmpSetOfFilteredCategoriesToSave.remove(category)
 
-    private fun getInitialFilterList(application: Application) =
-        mutableListOf(
-            FilterCategoryCard(
-                application.resources.getString(R.string.tv_cat_kids),
-                Category.KIDS,
-            ),
-            FilterCategoryCard(
-                application.resources.getString(R.string.tv_cat_adults),
-                Category.ADULTS,
-            ),
-            FilterCategoryCard(
-                application.resources.getString(R.string.tv_cat_events),
-                Category.EVENTS,
-            ),
-            FilterCategoryCard(
-                application.resources.getString(R.string.tv_cat_aged),
-                Category.AGED,
-            ),
-            FilterCategoryCard(
-                application.resources.getString(R.string.tv_cat_animals),
-                Category.ANIMALS,
-            ),
-        )
+    private fun getInitialFilterList(application: Application) = mutableListOf(
+        FilterCategoryCard(
+            application.resources.getString(R.string.tv_cat_kids),
+            Category.KIDS,
+        ),
+        FilterCategoryCard(
+            application.resources.getString(R.string.tv_cat_adults),
+            Category.ADULTS,
+        ),
+        FilterCategoryCard(
+            application.resources.getString(R.string.tv_cat_events),
+            Category.EVENTS,
+        ),
+        FilterCategoryCard(
+            application.resources.getString(R.string.tv_cat_aged),
+            Category.AGED,
+        ),
+        FilterCategoryCard(
+            application.resources.getString(R.string.tv_cat_animals),
+            Category.ANIMALS,
+        ),
+    )
 }
